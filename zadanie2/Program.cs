348f702a3dbdbf7fec48bdf4503b592ff3983578
Uliana <Uliana@mail.ru>
2022-09-27 11:38:28 +0300

    dz4

---
+++ zadanie2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Console.WriteLine("сумма цифр в числе");
+Console.Write("Введите число K ");
+int numberK = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberK){
+    
+    int counter = Convert.ToString(numberK).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberK - numberK % 10;
+      result = result + (numberK - advance);
+      numberK = numberK / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberK);
+Console.WriteLine("Сумма цифр " + sumNumber);
